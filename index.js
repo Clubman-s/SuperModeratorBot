a005b73a65bbd76d3185db5199bacb27da2e5f9a
Clubman-s <5871911@gmail.com>
2025-04-03 01:11:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,9 @@ const express = require('express');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const app = express();
 
-// Фикс для Vercel
 app.use(express.json());
-app.post('/api', (req, res) => {
-  console.log('📢 Получен запрос от Telegram');
-  bot.handleUpdate(req.body, res);
-});
+app.post('/api', (req, res) => bot.handleUpdate(req.body, res));
 
-// Простейший обработчик
-bot.on('text', (ctx) => {
-  console.log('💬 Сообщение:', ctx.message.text);
-  ctx.reply('Бот работает! Ваш текст: ' + ctx.message.text);
-});
+bot.on('text', (ctx) => ctx.reply('Бот жив. Получил: ' + ctx.message.text));
 
 module.exports = app;
